f7e794f969bc70ceb7afceeb3b426255fd432680
DilshanHarendra <harendracripto@gmail.com>
2022-03-16 01:44:05 +0530

    create about

---
+++ src/pages/about/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function About(){
+    return(<div>
+        <h1>About</h1>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab amet assumenda dolorum facere harum incidunt, labore natus tempora? Ab aut deleniti fugiat harum iste itaque provident quod repudiandae sed voluptatem.</p>
+    </div>)
+}export default About
