84047c016ad328d01dc810e065e1913237533262
DilshanHarendra <harendracripto@gmail.com>
2022-03-16 01:48:14 +0530

    add animation

---
+++ src/helper/animations.js
@@ -0,0 +1,14 @@
+export const sideBarAnimation={
+    init:{
+        width:0
+    },
+    animate:{
+        width:'400px',
+        transition:{delay:0.05}
+    },
+    exit:{
+        width:0,
+        opacity:0,
+
+    }
+}
