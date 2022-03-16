24fe8a22c3b033079e11383cbb041c4c220f8205
DilshanHarendra <harendracripto@gmail.com>
2022-03-17 00:24:30 +0530

    crate filters

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,53 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import ProductFilters from "./ProductFilters";
 import Products from "./Products";
+import {connect} from "react-redux";
+import {getProducts} from "../../store/actions/productsAction";
+import ProductsLoading from "./ProductsLoading";
 
 function Home(props){
+
+    const[loading,setLoading]=useState(true)
+    const [products,setProducts]=useState([])
+    useEffect(()=>{
+        if (props.products.products.length==0){
+            props.getProducts().then(res=>{
+                setProducts(res)
+                setLoading(false)
+            })
+        }else {
+                setProducts(props.products.products)
+                setLoading(false)
+        }
+
+    },[])
+
+    function sizeFilter(size){
+        setLoading(true)
+        if (size){
+            let filterProducts=props.products.products.filter(pro=>pro.details.size==size)
+            setProducts(filterProducts)
+            setLoading(false)
+        }else {
+            setProducts(props.products.products)
+            setLoading(false)
+        }
+
+    }
+
     return(<div className="flex items-start justify-between">
-            <ProductFilters/>
-            <Products/>
+            <ProductFilters sizeFilter={sizeFilter}/>
+        {
+            loading?
+                <ProductsLoading/>
+                :
+                <Products products={products}/>
+        }
+
      </div>)
-}export default Home
+}const mapStateToProps = state => ({
+    ...state
+});
+export default connect(mapStateToProps,{getProducts})(Home)
+
+
