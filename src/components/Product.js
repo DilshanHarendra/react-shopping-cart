34d8138fe476057f58eef613dbb45285d73ae96e
DilshanHarendra <harendracripto@gmail.com>
2022-03-17 00:26:40 +0530

    remove line

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,14 @@
 import React from 'react'
+import {connect} from "react-redux";
+import {addToCart} from "../store/actions/cartAction";
+import {openCart} from "../store/actions/siteSettingsAction";
 
-function Product({data}){
+function Product({data,cart,addToCart,openCart}){
+
+    function add(data){
+        addToCart(data)
+        openCart()
+    }
     return(
         <div className="w-full relative">
             {data.details.tag&&<span className="bg-black text-white text-xs  px-2 py-1 absolute top-0 right-0  block">{data.details.tag}</span>}
@@ -9,8 +17,12 @@ function Product({data}){
             <p className="text-center line-clamp-2 mt-4" style={{minHeight:'3.1rem'}}>{data.name}</p>
             <span className="bg-primary block mx-auto w-8" style={{minHeight:'2px'}}></span>
             <p className="text-center mt-3  ">$<span className="text-2xl font-semibold">{data.details.price}</span> </p>
-            <p className="text-center text-gray-500">or 9  x $1.21</p>
-            <button className="bg-black hover:bg-gray-800 text-white mt-5 px-4 py-2 w-full block">Add to cart</button>
+            <button onClick={()=> add(data)} className="bg-black hover:bg-gray-800 text-white mt-5 px-4 py-2 w-full block">Add to cart</button>
         </div>
     )
-}export default Product
+}const mapStateToProps = state => ({
+    ...state
+});
+export default connect(mapStateToProps,{addToCart,openCart})(Product)
+
+
