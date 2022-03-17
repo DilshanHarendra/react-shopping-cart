36cf51ef81fa1701c8467330fb482cad64ba6a34
DilshanHarendra <harendracripto@gmail.com>
2022-03-17 22:55:09 +0530

    add basename router

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/react-shopping-cart">
     <App />
   </BrowserRouter>,
   document.getElementById('root')
